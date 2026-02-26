58e91ef6e7e70e67047b0e60cc2719bb865046ea
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-26 17:01:02 +0100

    Arreglo formatos

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "SID - Sale Order Line Custom Fields (meta)",
+    "name": "sid_sale_line_custom_fields",
     "version": "15.0.1.0.0",
     "category": "Sales",
     "summary": "Modulo paraguas para compatibilidad. Agrupa dependencias funcionales de sale.order.line.",
-    "author": "SIDSA / Custom",
+    "author": "oscarsidsa81",
     "license": "LGPL-3",
     "depends": [
         "sid_sale_line_core",
